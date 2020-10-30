3dbbb5f3a96bd469b0346f7e50d93e600a0534a6
xtreme-steve-elliott <elliottst@vmware.com>
2020-10-30 16:12:32 -0400

    Welcome Controller stuff

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,22 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private final String welcomeMessage;
+
+    public WelcomeController(
+        @Value("${welcome.message}") String message
+    ) {
+        welcomeMessage = message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return welcomeMessage;
     }
 }
